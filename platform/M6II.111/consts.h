3c3ab810c2ddbe1961e3dd9526f1f5a0a81c2e83
stephen-e <33672591+reticulatedpines@users.noreply.github.com>
2025-11-14 07:31:47 +0000

    M6II: tested and confirmed LED address

--- platform/M6II.111/consts.h
+++ platform/M6II.111/consts.h
@@ -2,9 +2,9 @@
  *  EOS M6 Mark II consts
  */
 
-  #define CARD_LED_ADDRESS            0xD01300D4   /* maybe also 0xD01300D8/DC? */
-  #define LEDON                       0xD0002
-  #define LEDOFF                      0xC0003
+#define CARD_LED_ADDRESS            0xD01300D4   /* 0xD01300D8/DC: (no errors, no obvious reaction) */
+#define LEDON                       0xD0002
+#define LEDOFF                      0xC0003
 
 #define BR_DCACHE_CLN_1     0xe0040068   /* first call to dcache_clean, before cstart */
 #define BR_ICACHE_INV_1     0xe0040072   /* first call to icache_invalidate, before cstart */
